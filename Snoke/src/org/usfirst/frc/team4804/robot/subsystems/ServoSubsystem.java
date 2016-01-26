6d2a97eaca8db6f392c555f37fd8e781334be7ef
portpiratech <portpiratech@gmail.com>
2016-01-25 20:51:56 -0600

    Cleaned up servo subsystem skeleton
    -Tommy

--- Snoke/src/org/usfirst/frc/team4804/robot/subsystems/ServoSubsystem.java
+++ Snoke/src/org/usfirst/frc/team4804/robot/subsystems/ServoSubsystem.java
@@ -3,24 +3,25 @@ package org.usfirst.frc.team4804.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class ServoSubsystem extends Subsystem {
-
-	public void turnRight()
-	{
-		
-		
+	
+	// Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	public ServoSubsystem() {
+		super();
+		// create servo object
+	}
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new ExampleCommand());
+    }
+    
+	public void turnRight() {
 		
 	}
 	
-	public void turnLeft()
-	{
-		
-		
-		
-	}
-	
-	@Override
-	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
+	public void turnLeft() {
 		
 	}
 	
